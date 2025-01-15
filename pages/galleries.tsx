a78bb02f9226c6a538f5ab139f284dfab69d08ce
Nasyath Faykar <nasyath0194@stmik-wp.ac.id>
2025-01-16 00:10:46 +0700

    chore: back to awal

--- pages/galleries.tsx
+++ pages/galleries.tsx
@@ -1,10 +1,6 @@
-import { Box, SimpleGrid, Heading, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid, Image, Heading, Text } from "@chakra-ui/react";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Pagination  from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 const Galleries: NextPage = () => {
   // Array foto-foto galeri
@@ -15,22 +11,7 @@ const Galleries: NextPage = () => {
     "/images/memories4.jpg",
     "/images/memories5.jpg",
     "/images/memories6.jpg",
-    "/images/memories7.jpg",
-    "/images/memories8.jpg",
-    "/images/memories9.jpg",
-    "/images/memories10.jpg",
   ];
-
-  // Membagi foto menjadi kelompok (chunk) 4
-  const chunkSize = 4;
-  const groupedPhotos = photos.reduce<string[][]>((result, photo, index) => {
-    const chunkIndex = Math.floor(index / chunkSize);
-    if (!result[chunkIndex]) {
-      result[chunkIndex] = [];
-    }
-    result[chunkIndex].push(photo);
-    return result;
-  }, []);
 
   return (
     <>
@@ -54,37 +35,18 @@ const Galleries: NextPage = () => {
           A collection of unforgettable memories
         </Text>
 
-        {/* Grid untuk carousel */}
+        {/* Grid untuk galeri */}
         <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={5}>
-          {groupedPhotos.map((group, index) => (
-            <Box
-              key={index}
-              overflow="hidden"
-              borderRadius="md"
-              boxShadow="md"
-              p={3}
-              bgColor="gray.50"
-            >
-              <Swiper
-                modules={[Pagination]} // Pastikan Pagination ada di sini
-                pagination={{ clickable: true }}
-                spaceBetween={10}
-                slidesPerView={1}
-                >
-                {group.map((photo, idx) => (
-                    <SwiperSlide key={idx}>
-                    <Box
-                        as="img"
-                        src={photo}
-                        alt={`Memory ${index * chunkSize + idx + 1}`}
-                        objectFit="cover"
-                        w="100%"
-                        h="200px"
-                        borderRadius="md"
-                    />
-                    </SwiperSlide>
-                ))}
-                </Swiper>
+          {photos.map((photo, index) => (
+            <Box key={index} overflow="hidden" borderRadius="md" boxShadow="md">
+              <Image
+                src={photo}
+                alt={`Memory ${index + 1}`}
+                objectFit="cover"
+                w="100%"
+                h="200px"
+                _hover={{ transform: "scale(1.1)", transition: "all 0.3s" }}
+              />
             </Box>
           ))}
         </SimpleGrid>
