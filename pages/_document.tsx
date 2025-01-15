a2a1c8ebc2b8c3dcd797932a998f09ceb8cb59d2
Nasyath Faykar <nasyath0194@stmik-wp.ac.id>
2025-01-15 22:30:46 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <link rel="apple-touch-icon" href="/logo-upt.png"></link>
         <meta name="theme-color" content="#FF0080" />
       </Head>
       <body>
